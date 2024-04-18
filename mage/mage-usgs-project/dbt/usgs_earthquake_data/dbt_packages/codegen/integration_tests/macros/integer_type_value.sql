b4733b28ed2f189ae61e8edcee8c20a77acdce6d
Lottie Jane Pollard <lottiejanepollard@gmail.com>
2024-04-18 23:32:43 +0000

    final commit

--- mage/mage-usgs-project/dbt/usgs_earthquake_data/dbt_packages/codegen/integration_tests/macros/integer_type_value.sql
+++
@@ -1,9 +0,0 @@
-{%- macro integer_type_value() -%}
-{%- if target.type == "snowflake" -%}
-number
-{%- elif target.type == "bigquery" -%}
-int64
-{%- else -%}
-integer
-{%- endif -%}
-{%- endmacro -%}
